2b47df3954c04f99f7f90c30e89e827759781c3a
igor666777 <gulik_1986@mail.ru>
2022-03-21 15:53:38 +0300

    Fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,18 @@ import logo from './logo.svg'
 import './App.css'
 import ColorButton from './components/ColorButton';
 import CatFact from './components/CatFact';
-
+import Back from './components/Back';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* {prop1}
-        <img src={logo} className="App-logo" alt="logo" /> */}
+        {/* {prop1} */}
+        <img src={logo} className="App-logo" alt="logo" />
+
         <CatFact />
         <ColorButton />
+        <Back />
       </header>
     </div>
   )
--- src/components/Back.jsx
+++ src/components/Back.jsx
@@ -1,11 +1,22 @@
-// import { useState, useEffect } from "react";
-
+import { useState, useEffect } from "react";
 
 export default function Back() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
+    const [errors, setErrors] = useState([]);
+    const [isDisabled, setIsDisabled] = useState(true);
 
+    const handleSubmit = () => {
+        setErrors([])
+        if (!email) {
+            setErrors((prev) => [...prev, 'email']);
+        }
+        if (!password) {
+            setErrors((prev) => [...prev, 'password']);
+        }
+        if (errors.length) return;
 
-    function getPassword(email, password) {
         fetch('https://tms-js-pro-back-end.herokuapp.com/api/users/signin', {
             method: 'POST',
             headers: {
@@ -16,125 +27,70 @@ export default function Back() {
                 email,
                 password,
             }),
-        }).then((res) => {
-            if (res.ok) {
-                console.log(password);
-            }
-        });
-    }
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                sessionStorage.token = data.token;
+            });
+    };
 
-    const email = 'igorg@mail.com';
 
-    for (let i = 0; i <= 99; i += 1) {
-        const myPass = `${i}`.padStart(2, 0);
+    useEffect(() => {
+        setIsDisabled(!!errors.length);
+    }, [errors]);
+    return (
 
-        getPassword(email, myPass);
-    }
+        <div
+            style={{
+                height: '100vh',
+                width: '100vw',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+            }}
+        >
+            {!sessionStorage.token ? (
+                <div
+                    style={{
+                        display: 'grid',
+                        gridTemplateColumns: '1fr',
+                        gap: 16,
+                    }}
+                >
+                    <input
+                        required
+                        type='email'
+                        value={email}
+                        onChange={(e) => {
+                            setEmail(e.target.value);
+                            setErrors((prev) => prev.filter((error) => error !== 'email'));
+                            if (e.target.value) setErrors((prev) => [...prev, 'email']);
+                        }}
+                    />
+                    <input
+                        required
+                        type='password'
+                        value={password}
+                        onChange={(e) => {
+                            setPassword(e.target.value);
+                            setErrors((prev) => prev.filter((error) => error !== 'password'));
+                            if (e.target.value) setErrors((prev) => [...prev, 'password']);
+                        }}
+                    />
+                    {!!errors.length && (
+                        <span style={{ color: 'red' }}>{`${errors.join(
+                            ' and '
+                        )} required`}</span>
+                    )}
+                    <button type="button" onClick={handleSubmit} disabled={isDisabled}>
+                        sign in
+                    </button>
+                </div>
+            ) : (<button type="button" onClick={(e) => (sessionStorage.token = '')}>sign out</button>
+            )}
+        </div>
+    );
 }
-//     const [email, setEmail] = useState('');
-//     const [password, setPassword] = useState('');
-
-//     const [errors, setErrors] = useState([]);
-//     const [isDisabled, setIsDisabled] = useState(true);
-
-//     const handleSubmit = () => {
-//         setErrors([])
-//         if (!email) {
-//             setErrors((prev) => [...prev, 'email']);
-//         }
-//         if (!password) {
-//             setErrors((prev) => [...prev, 'password']);
-//         }
-//         if (errors.length) return;
-
-//         function getPassword(email, password) {
-//             fetch('https://tms-js-pro-back-end.herokuapp.com/api/users/signin', {
-//                 method: 'POST',
-//                 headers: {
-//                     Accept: 'application/json',
-//                     'Content-Type': 'application/json',
-//                 },
-//                 body: JSON.stringify({
-//                     email,
-//                     password,
-//                 }),
-//             }).then((res) => {
-//                 if (res.ok) {
-//                     console.log(password);
-//                 }
-//                 // .then((response) => response.json())
-//                 // .then((data) => {
-//                 //     sessionStorage.token = data.token;
-//                 // });
-
-//             });
-//         }
-
-//         const email = 'igorg@mail.com';
-
-//         for (let i = 0; i <= 99; i += 1) {
-//             const myPass = i.toString().padStart(2, 0);
-//             getPassword(email, myPass);
-//         }
-//     }
-
-
-//     useEffect(() => {
-//         setIsDisabled(!!errors.length);
-//     }, [errors]);
-//     return (
-
-//         <div
-//             style={{
-//                 height: '100vh',
-//                 width: '100vw',
-//                 display: 'flex',
-//                 alignItems: 'center',
-//                 justifyContent: 'center',
-//             }}
-//         >
-//             {!sessionStorage.token ? (
-//                 <div
-//                     style={{
-//                         display: 'grid',
-//                         gridTemplateColumns: '1fr',
-//                         gap: 16,
-//                     }}
-//                 >
-//                     <input
-//                         required
-//                         type='email'
-//                         value={email}
-//                         onChange={(e) => {
-//                             setEmail(e.target.value);
-//                             setErrors((prev) => prev.filter((error) => error !== 'email'));
-//                             if (e.target.value) setErrors((prev) => [...prev, 'email']);
-//                         }}
-//                     />
-//                     <input
-//                         required
-//                         type='password'
-//                         value={password}
-//                         onChange={(e) => {
-//                             setPassword(e.target.value);
-//                             setErrors((prev) => prev.filter((error) => error !== 'password'));
-//                             if (e.target.value) setErrors((prev) => [...prev, 'password']);
-//                         }}
-//                     />
-//                     {!!errors.length && (
-//                         <span style={{ color: 'red' }}>{`${errors.join(
-//                             ' and '
-//                         )} required`}</span>
-//                     )}
-//                     <button type="button" onClick={handleSubmit} disabled={isDisabled}>
-//                         sign in
-//                     </button>
-//                 </div>
-//             ) : (<button type="button" onClick={(e) => (sessionStorage.token = '')}>sign out</button>
-//             )}
-//         </div>
-//     );
-
 
 
 
